95541bb5179272f832065dc8f837b95f807e356b
JanTkacSK <jan.tkac.sk@gmail.com>
2025-10-13 14:19:02 +0200

    commit

--- PSZabbix-7/PSZabbix-7.psm1
+++ PSZabbix-7/PSZabbix-7.psm1
@@ -2313,7 +2313,7 @@ function Get-ZXTrigger {
     if ($HostGroupID){
         $PSObj.params | Add-Member -MemberType NoteProperty -Name "groupids" -Value $HostGroupID
     }
-    if ($HostGroupID){
+    if ($TemplateID){
         $PSObj.params | Add-Member -MemberType NoteProperty -Name "templateids" -Value $TemplateID
     }
     if ($Description){
@@ -3762,6 +3762,73 @@ function Update-ZXService {
     }
     
 }
+function Get-ZXUserMacro {
+    param(
+        [array]$HostID,
+        [array]$HostGroupID,
+        [array]$TemplateID,
+        [string]$Description,
+        [array]$Output,
+        [switch]$IncludeHosts,
+        [switch]$IncludeHostGroups,
+        [switch]$WhatIf,
+        [switch]$Inherited,        
+        [string]$Limit
+    )
+
+    #Basic PS Object wich will be edited based on the used parameters and finally converted to json
+    $PSObj = New-ZXApiRequestObject -Method "usermacro.get"
+
+    #Validate parameters
+    if (!$Output){
+        [string]$Output = "extend"
+    }
+    elseif($Output -contains "extend") {
+        [string]$Output = "extend"
+    }
+
+    #Get the item for the hosts with the specified IDs
+    if ($HostID){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "hostids" -Value $HostID
+    }
+
+    # Add "selectHosts" parameter to return all hosts linked tho the templates.
+    if ($IncludeHosts){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "selectHosts" -Value @("host","name","description")
+    }
+
+    if ($HostGroupID){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "groupids" -Value $HostGroupID
+    }
+    if ($TemplateID){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "templateids" -Value $TemplateID
+    }
+    if ($Description){
+        AddFilter -PropertyName "description" -PropertyValue $Description
+    }
+    if ($Inherited){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "inherited" -Value "true"
+    }
+    if($Limit){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "limit" -Value $Limit
+    }
+
+    $PSObj.params | Add-Member -MemberType NoteProperty -Name "output" -Value $Output
+    
+    $Json =  $PSObj | ConvertTo-Json -Depth 3
+
+    #Show JSON Request if -Whatif switch is used
+    If ($WhatIf){
+        Write-JsonRequest
+    }
+    
+    #Make the API call
+    if(!$WhatIf){
+        $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
+        Resolve-ZXApiResponse -Request $Request
+    }
+
+}
 Export-ModuleMember -Function `
     Add-ZXHostToGroup, `
     Add-ZXHostNameSuffix, `
@@ -3789,6 +3856,7 @@ Export-ModuleMember -Function `
     Get-ZXTemplate, `
     Get-ZXTrigger, `
     Get-ZXTriggerPrototype, `
+    Get-ZXUserMacro, `
     Invoke-ZXTask, `
     New-ZXHost, `
     New-ZXProblemTagList, `
