d5097147827fd2a45ce72dd720ae26493f00ada4
JanTkacSK <jan.tkac.sk@gmail.com>
2025-11-26 10:04:29 +0100

    commit

--- PSZabbix-7/PSZabbix-7.psm1
+++ PSZabbix-7/PSZabbix-7.psm1
@@ -3035,7 +3035,7 @@ function New-ZXTokenSession{
         #Get the data from the Tokens.txt, filter out the entry with the same URL you have entered in case it exists.
         #This way you can enter the same url again and the record with this url will be overwritten
         $LogonData = @()
-        $LogonData += Get-Content -Raw $SaveLocation | ConvertFrom-Json | Where-Object {$_.URL -ne $NewObj.URL }
+        $LogonData += Get-Content -Raw $SaveLocation | ConvertFrom-Json | Where-Object {$_.URL.Trim() -ne $NewObj.URL.Trim() }
         #Add the data you entered into read-host prompt
         $LogonData += $NewObj
         #Add an Id to each object
